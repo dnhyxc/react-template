46d2badf438e7931560b365589d9434128bdb8b8
dnhyxc <2640559435@qq.com>
2022-06-12 10:41:56 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./router"
+import App from "./router";
 import "./index.less";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(<App />)
